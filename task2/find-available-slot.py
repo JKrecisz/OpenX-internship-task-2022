3e3c6b0e145e32772f7ac1475ac78211b19372af
JKrecisz <jakub.krecisz@icloud.com>
2022-04-27 16:11:04 +0200

    Second task descriptions and task1 python file created

--- task2/find-available-slot.py
+++ task2/find-available-slot.py
@@ -1,40 +1,55 @@
+"""
+----------------------------------------------------------------------
+    The script print out the soonest date in the future when at least
+    desired amount of people are available for given amount of time.
+----------------------------------------------------------------------
+    Autor: Jakub Krecisz                            Krakow, 27.04.2022
+----------------------------------------------------------------------
+"""
+
 import argparse
 import datetime
 import glob
 import re
 
+# Defining arguments for retrieving the number of people, availability in minutes and calendars path
 parser = argparse.ArgumentParser(description='Find available slot for a specific number of people and time')
+
 parser.add_argument('--duration-in-minutes', type=int, required=True, metavar='',
                     help='Availability of people (minutes)')
 parser.add_argument('--minimum-people', type=int, required=True, metavar='', help='Minimum number of people')
 parser.add_argument('--calendars', required=True, metavar='', help='Name of the date folder')
+
 args = parser.parse_args()
 
+# Variables in which we keep paths for calendars, duration in datetime type and current date
 personsCalendars = glob.glob(f'.{args.calendars}/*.txt')
 minutesDuration = datetime.timedelta(minutes=args.duration_in_minutes)
 currentDate = datetime.datetime.now()
 
 
+# A function that for a specific day checks minute by minute whether our availability slot fits
 def findSlot(date, minNumOfPeople, calendarsPaths, minutes):
-    busyTimes = []
+    busyTimes = []  # All busy hours for all people
     dateRegex = f'{date.strftime("%Y-%m-%d")} \d\d:\d\d:\d\d - {date.strftime("%Y-%m-%d")} \d\d:\d\d:\d\d|' \
                 f'{date.strftime("%Y-%m-%d")}'
+    availableTime = datetime.datetime(date.year, date.month, date.day, date.hour, date.minute)
 
     if minNumOfPeople <= 0:
         return '--minimum-people must be >= 1'
-
-    availableTime = datetime.datetime(date.year, date.month, date.day, date.hour, date.minute)
 
     for filePath in calendarsPaths:
         busyTimes.append(re.findall(dateRegex, open(filePath).read()))
 
     if len(busyTimes) < minNumOfPeople:
-        return f"Not enough people in calendars, currently in calendars is {len(busyTimes)} people"
+        return f"Not enough people in calendars, currently in calendars: {len(busyTimes)} people"
 
     if busyTimes.count([]) >= minNumOfPeople:
         return datetime.datetime(date.year, date.month, date.day)
 
     counter = 0
+
+    # We check for each possible slot whether enough people are available
     while availableTime.day == date.day:
         for personsBusyTimes in busyTimes:
             counter += findEachDaySlot(personsBusyTimes, minutes, availableTime)
@@ -44,6 +59,7 @@ def findSlot(date, minNumOfPeople, calendarsPaths, minutes):
         counter = 0
 
 
+# The function checks if the specified availability slot falls within the specified hours for the period
 def findEachDaySlot(personsTimes, minutes, startingTime):
     endingTime = startingTime + minutes
     beginningOfTheDay = datetime.datetime(startingTime.year, startingTime.month, startingTime.day)
@@ -53,6 +69,7 @@ def findEachDaySlot(personsTimes, minutes, startingTime):
         return 1
 
     for time in personsTimes:
+        # check by ':' because we take only those values where the person is occupied for a certain number of hours
         if ':' in time:
             busyTimeStarts = datetime.datetime(int(time[0:4]), int(time[5:7]), int(time[8:10]),
                                                int(time[11:13]), int(time[14:16]), int(time[18:20]))
